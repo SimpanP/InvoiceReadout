8c274bd9f7f261f5e3b01ba1dca0a8921118ccce
SimpanP <113165536+SimpanP@users.noreply.github.com>
2023-11-22 11:38:08 +0100

    update db_connection.py
    upload_pdf and download_pdf method

--- backend/InvoiceReader/InvoiceReader/db_connection.py
+++ backend/InvoiceReader/InvoiceReader/db_connection.py
@@ -1,11 +1,28 @@
 import pymongo
 from pymongo import MongoClient
 import dns
+from gridfs import GridFS
 
 cluster = pymongo.MongoClient("mongodb+srv://SimFil:Sommar2021@cluster0.k6mlfru.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["invoiceReader"]
 collection = db["invoiceReader"]
 
-post = {"_id":0, "name": "simon", "score": 5}
 
-collection.insert_one(post)
+# Create a GridFS object
+fs = GridFS(db)
+
+def upload_pdf(file_path, filename):
+    # Open the PDF file in binary mode
+    with open(file_path, 'rb') as pdf_file:
+        # Store the file in GridFS
+        pdf_id = fs.put(pdf_file, filename=filename)
+    
+    return pdf_id
+
+def download_pdf(pdf_id, destination_path):
+    try:
+        # Retrieve the PDF file from GridFS
+        with open(destination_path, 'wb') as pdf_file:
+            fs.get(pdf_id).readinto(pdf_file)
+    except NoFile:
+        print(f"No file found with id: {pdf_id}")
